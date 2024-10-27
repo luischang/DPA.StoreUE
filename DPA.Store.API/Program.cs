cdf5462951e9d5d9c0578c497324b5f11967b29f
Luis Chang <luis.chang@outlook.com>
2024-10-26 21:04:46 -0500

    CORS

--- DPA.Store.API/Program.cs
+++ DPA.Store.API/Program.cs
@@ -29,6 +29,20 @@ builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddSharedInfrastructure(_config);
 
 builder.Services.AddControllers();
+
+//ADD CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder//.WithOrigins("http://www.midominiofrontend.com")
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -43,7 +57,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
+app.UseCors();
 app.MapControllers();
 
 app.Run();
